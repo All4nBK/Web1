7d5492561cc64d2fa7e1a2db00df3851775e8d16
Alan Dev <101974432+All4nBK@users.noreply.github.com>
2023-11-01 09:38:45 -0300

    Up de arquivos
    Up

---
+++ script.js
@@ -0,0 +1,8 @@
+let button = document.getElementById('no');
+button.addEventListener('click', function () {
+    alert("Você realmente não quer comprar esse produto incrível")
+
+})
+button.addEventListener('mouseover', function () {
+    alert("Você realmente não quer comprar esse produto incrível")
+})
